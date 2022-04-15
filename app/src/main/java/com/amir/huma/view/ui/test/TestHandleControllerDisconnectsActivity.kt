89adce44644bb8b3915a09f9f89bd529a294684d
amirb938 <amirb938@gmail.com>
2022-04-16 01:05:15 +0430

    Test Handle Controller Disconnects

---
+++ app/src/main/java/com/amir/huma/view/ui/test/TestHandleControllerDisconnectsActivity.kt
@@ -0,0 +1,12 @@
+package com.amir.huma.view.ui.test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.amir.huma.R
+
+class TestHandleControllerDisconnectsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_handle_controller_disconnects)
+    }
+}
